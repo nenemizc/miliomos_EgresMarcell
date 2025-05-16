352de2afab402ebe358dc0026b6c0774f1d6db9f
nenemizc <emarcellm@gmail.com>
2025-05-16 13:45:44 +0200

    Kerdes classok

--- miliomos/Kerdes.cs
+++ miliomos/Kerdes.cs
@@ -8,19 +8,25 @@ namespace miliomos
 {
 	internal class Kerdes
 	{
-		string kerdes;
+		string talany;
 		string[] valasz;
 		string helyes;
 		string kat;
 		int szint;
 
-		public Kerdes(string kerdes, string[] valasz, string helyes, string kat, int szint)
-		{
-			this.kerdes = kerdes;
-			this.valasz = valasz;
-			this.helyes = helyes;
-			this.kat = kat;
-			this.szint = szint;
-		}
-	}
+        public Kerdes(string talany, string[] valasz, string helyes, string kat, int szint)
+        {
+            this.talany = talany;
+            this.valasz = valasz;
+            this.helyes = helyes;
+            this.kat = kat;
+            this.szint = szint;
+        }
+
+        public string Talany { get => talany; set => talany = value; }
+        public string[] Valasz { get => valasz; set => valasz = value; }
+        public string Helyes { get => helyes; set => helyes = value; }
+        public string Kat { get => kat; set => kat = value; }
+        public int Szint { get => szint; set => szint = value; }
+    }
 }
--- miliomos/MasikKerdes.cs
+++ miliomos/MasikKerdes.cs
@@ -8,5 +8,22 @@ namespace miliomos
 {
 	internal class MasikKerdes
 	{
-	}
+        string kerdes;
+		string[] valasz;
+		string helyes;
+		string kat;
+
+        public MasikKerdes(string kerdes, string[] valasz, string helyes, string kat)
+        {
+            this.kerdes = kerdes;
+            this.valasz = valasz;
+            this.helyes = helyes;
+            this.kat = kat;
+        }
+
+        public string Kerdes { get => kerdes;}
+        public string[] Valasz { get => valasz;}
+        public string Helyes { get => helyes;}
+        public string Kat { get => kat;}
+    }
 }
